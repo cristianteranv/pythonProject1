6243b40d8ca910a0b077b9fc9751ecd5fbf4c44c
Cristian Teran <cristianteranv@gmail.com>
2021-11-11 10:20:25 -0500

    good calc

--- main.py
+++
@@ -1,8 +0,0 @@
-""" Content of calculator """
-def inc(x_value):
-    """ increments by 1 """
-    return x_value + 1
-
-def test_answer():
-    """ tests that inc() is working properly """
-    assert inc(4) == 5
