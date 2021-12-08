12d53ba69f3cfd314e7b55f2e6c783d5e7c429f2
Cristian Teran <cristianteranv@gmail.com>
2021-12-08 11:13:52 -0500

    flashing

--- app/app.py
+++ app/app.py
@@ -1,13 +1,16 @@
 """A simple flask web app"""
-from flask import Flask, request
+from flask import Flask, request,flash
 from flask import render_template
 import pandas
 from calculator.calculator import Calculator
+
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
 @app.route("/")
 def index():
     """index  Route Response"""
+    flash("flash test")
     return render_template("index.html")
 
 @app.route("/calculate", methods=["GET","POST"])
